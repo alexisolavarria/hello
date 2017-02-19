836ad632f6374d7e6b325f32618c985635f0978b
Alexis Olavarria <alexis.g.s.olavarria@gmail.com>
2017-02-19 20:38:53 +1100

    Initial commit

---
+++ hello.rb
@@ -0,0 +1,13 @@
+# hello.rb
+# asks for your name then greets you by name
+
+# what a shit first program 
+print "\nComputer: Hello, What is your name? \n"
+
+print "Me:       "
+
+name = gets.strip
+
+puts "\nComputer: Hello, #{name}!\n\n"
+
+#	print "Okay #{name}, what's your favourite colour? "
